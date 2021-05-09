a2722bc3a13d4a251ca390711b4f64480c53d74a
dr3dd <dr3dd1337@gmail.com>
2021-05-09 12:55:11 +0530

    added some code.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ url = "https://test.com"
 jwt = "eyJhbGciPiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 user = "admin"
 password = "qwerty@123"
-
+awsKey = "ASIAZIQ5SEG4MYAZQHT5"
 username = input("Who are you? ")
 if username == user:
     pas = input("Give me your password!! ")
